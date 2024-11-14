639e6a39ed6b91ce80154b6104da79ae20432c20
Jorge Greñas <J4ck.DevCode@gmail.com>
2024-11-13 20:36:43 -0500

    Export custom delegate

--- lib/flutter_translate.dart
+++ lib/flutter_translate.dart
@@ -3,6 +3,7 @@ library flutter_translate;
 export 'src/utils/utils.dart';
 export 'src/services/localization.dart';
 export 'src/delegates/localization_delegate.dart';
+export 'src/delegates/custom_localization_delegate.dart';
 export 'src/widgets/localization_provider.dart';
 export 'src/widgets/localized_app.dart';
 export 'src/widgets/localized_app_state.dart';

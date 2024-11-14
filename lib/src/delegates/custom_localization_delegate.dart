8b33b987219b25141fa2c18769e9d4d654e1283c
Jorge Greñas <J4ck.DevCode@gmail.com>
2024-11-13 21:04:17 -0500

    Updated custom_localizations

--- lib/src/delegates/custom_localization_delegate.dart
+++ lib/src/delegates/custom_localization_delegate.dart
@@ -1,85 +1,65 @@
-import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_translate/src/validators/configuration_validator.dart';
-import 'package:intl/intl.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:flutter_translate/src/services/locale_service.dart';
 import 'package:flutter_translate/src/constants/constants.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter_translate/src/validators/configuration_validator.dart';
 
-class CustomLocalizationDelegate extends LocalizationsDelegate<Localization> {
-  final String basePath;
+class CustomLocalizationDelegate {
+  Locale? _currentLocale;
   final Locale fallbackLocale;
   final List<Locale> supportedLocales;
   final Map<Locale, String> supportedLocalesMap;
+  final ITranslatePreferences? preferences;
+  LocaleChangedCallback? onLocaleChanged;
+
+  Locale get currentLocale => _currentLocale!;
 
   CustomLocalizationDelegate({
-    required this.basePath,
     required this.fallbackLocale,
     required this.supportedLocales,
     required this.supportedLocalesMap,
+    this.preferences,
   });
 
-  Future<File> _getTranslationFile(Locale locale) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final filePath = '${directory.path}/translations/${locale.languageCode}.json';
-    final file = File(filePath);
+  Future changeLocale(Locale newLocale) async {
+    var isInitializing = _currentLocale == null;
 
-    // Si el archivo no existe, devolver el archivo en inglés por defecto
-    if (!await file.exists()) {
-      print("Translation file not found. Returning default English.");
-      return File('${directory.path}/translations/en.json');
+    var locale = LocaleService.findLocale(newLocale, supportedLocales) ?? fallbackLocale;
+
+    if (_currentLocale == locale) return;
+
+    var localizedContent = await LocaleService.getLocaleContent(locale, supportedLocalesMap);
+
+    Localization.load(localizedContent);
+
+    _currentLocale = locale;
+
+    if (!isInitializing && preferences != null) {
+      await preferences!.savePreferredLocale(locale);
     }
 
-    return file;
+    if (onLocaleChanged != null) {
+      await onLocaleChanged!(locale);
+    }
   }
 
-  @override
   Future<Localization> load(Locale newLocale) async {
-    // Si la localización actual es diferente de la nueva, cambian las traducciones
-    await changeLocale(newLocale);
+    if (currentLocale != newLocale) {
+      await changeLocale(newLocale);
+    }
+
     return Localization.instance;
   }
 
-  Future changeLocale(Locale newLocale) async {
-    var isInitializing = false; // Asumimos que es la primera vez
-
-    // Verifica si la nueva localidad es compatible
-    var locale = LocaleService.findLocale(newLocale, supportedLocales) ?? fallbackLocale;
-
-    var localizedContent = await _loadLocaleContent(locale);
-    Localization.load(localizedContent);
-
-    // Si no estamos inicializando, podemos guardar las preferencias
-    if (!isInitializing) {
-      // Guardar preferencias, si tienes el servicio de preferencias habilitado.
-    }
-
-    Intl.defaultLocale = locale.languageCode;
-  }
-
-  Future<Map<String, dynamic>> _loadLocaleContent(Locale locale) async {
-    try {
-      final file = await _getTranslationFile(locale);
-      final content = await file.readAsString();
-      return jsonDecode(content);
-    } catch (e) {
-      print("Error loading translation file: $e");
-      return {}; // Si hay un error, devolver un mapa vacío
-    }
-  }
-
-  @override
   bool isSupported(Locale? locale) => locale != null;
 
-  @override
-  bool shouldReload(LocalizationsDelegate<Localization> old) => true;
+  bool shouldReload(LocalizationsDelegate old) => true;
 
   static Future<CustomLocalizationDelegate> create({
     required String fallbackLocale,
     required List<String> supportedLocales,
     String basePath = Constants.localizedAssetsPath,
+    ITranslatePreferences? preferences,
   }) async {
     WidgetsFlutterBinding.ensureInitialized();
 
@@ -90,12 +70,47 @@ class CustomLocalizationDelegate extends LocalizationsDelegate<Localization> {
     ConfigurationValidator.validate(fallback, locales);
 
     var delegate = CustomLocalizationDelegate(
-      basePath: basePath,
       fallbackLocale: fallback,
       supportedLocales: locales,
       supportedLocalesMap: localesMap,
+      preferences: preferences,
     );
 
+    if (!await delegate._loadPreferences()) {
+      await delegate._loadDeviceLocale();
+    }
+
     return delegate;
+  }
+
+  Future<bool> _loadPreferences() async {
+    if (preferences == null) return false;
+
+    Locale? locale;
+
+    try {
+      locale = await preferences!.getPreferredLocale();
+    } catch (e) {
+      return false;
+    }
+
+    if (locale != null) {
+      await changeLocale(locale);
+      return true;
+    }
+
+    return false;
+  }
+
+  Future _loadDeviceLocale() async {
+    try {
+      var locale = getCurrentLocale();
+
+      if (locale != null) {
+        await changeLocale(locale);
+      }
+    } catch (e) {
+      await changeLocale(fallbackLocale);
+    }
   }
 }
